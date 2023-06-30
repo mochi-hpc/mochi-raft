f4d77dac9aa19ec11d949232b0e6cb84e768ee0e
Matthieu Dorier <mdorier@anl.gov>
2023-06-30 16:00:38 +0000

    making the test work

--- tests/python/test-mpi.py
+++ tests/python/test-mpi.py
@@ -3,18 +3,20 @@ import random
 from mpi4py import MPI
 from pymargo.core import Engine
 import pymargo.core
-from pymraft import ServerInfo, Raft
+from pymraft import ServerInfo, Raft, RaftException
 import pymraft
 
 
 class MyStateMachine():
 
-    def __init__(self):
+    def __init__(self, rank: int):
         self.content = b''
+        self.rank = rank
 
     def apply(self, command: memoryview) -> None:
         command = bytes(command)
-        print(f"Received command {command}")
+        print(f"Rank {self.rank} received command {command}")
+        sys.stdout.flush()
         self.content += command + b'-'
 
     def snapshot(self) -> bytes:
@@ -27,26 +29,38 @@ class MyStateMachine():
 def test(engine: Engine, servers: list[ServerInfo]):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    fsm = MyStateMachine()
+    fsm = MyStateMachine(rank)
     raft = Raft(mid=engine.mid, id=rank+1, fsm=fsm)
     raft.bootstrap(servers)
     comm.barrier()
     raft.start()
     comm.barrier()
+    print("Waiting a bit to make sure a leader is elected")
+    engine.sleep(2000)
     print("Starting to apply commands")
-    #for i in range(0, 10):
-    #    engine.sleep(random.randint(1, 200))
-    #    command = f"{rank}/{i}".encode("ascii")
-    #    raft.apply(command)
+    for i in range(0, 10):
+        engine.sleep(random.randint(100, 200))
+        command = f"{rank}/{i}".encode("ascii")
+        try:
+            raft.apply(command)
+        except RaftException as e:
+            print(e)
+            break
     print("Sleeping to make sure everyone finished")
     engine.sleep(2000)
+    print("Checking that everyone got the same state")
+    states = comm.allgather(fsm.content)
+    if len(states) > 1:
+        for i in range(1, len(states)):
+            assert states[i] == states[i-1]
+    print("Done!")
 
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     random.seed(rank)
     with Engine('ofi+tcp', mode=pymargo.core.server, use_progress_thread=True) as engine:
-        engine.logger.set_log_level(pymargo.logging.level.trace)
+        #engine.logger.set_log_level(pymargo.logging.level.trace)
         self_addr = str(engine.addr())
         addresses = comm.allgather(self_addr)
         servers = [ServerInfo(id=i+1, address=address, role=pymraft.role.Voter)
