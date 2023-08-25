d05eb5a2fabf3a1c7f50788f7062ce4348a2e7d2
Matthieu Dorier <mdorier@anl.gov>
2023-08-25 16:04:38 +0000

    implementing worker

--- tests/generic-test.cpp
+++ tests/generic-test.cpp
@@ -5,8 +5,11 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <filesystem>
+#include <string_view>
 #include <map>
 #include <thallium.hpp>
+#include <thallium/serialization/stl/vector.hpp>
+#include <thallium/serialization/stl/string.hpp>
 #include <mochi-raft.hpp>
 #include <tclap/CmdLine.h>
 #define SOL_ALL_SAFETIES_ON 1
@@ -26,7 +29,6 @@ struct Options {
 
 static void parseCommandLine(int argc, char** argv, Options& options);
 static void runMaster(const Options& options);
-[[noreturn]] static void runWorker(const Options& options, tl::engine engine);
 
 // ----------------------------------------------------------------------------
 // Main function
@@ -40,13 +42,131 @@ int main(int argc, char** argv) {
 }
 
 // ----------------------------------------------------------------------------
+// Worker structure
+// ----------------------------------------------------------------------------
+
+struct FSM : public mraft::StateMachine {
+
+    FSM(raft_id id)
+    : id(id) {}
+
+    void apply(const struct raft_buffer* buf, void** result) override {
+        content.append(std::string_view{(const char*)buf->base, buf->len});
+    }
+
+    void snapshot(struct raft_buffer* bufs[], unsigned* n_bufs) override {
+        *bufs   = static_cast<raft_buffer*>(raft_malloc(sizeof(**bufs)));
+        *n_bufs = 1;
+        (*bufs)[0].base = strdup(content.c_str());
+        (*bufs)[0].len  = content.size() + 1;
+    }
+
+    void restore(struct raft_buffer* buf) override {
+        content.assign(std::string_view{(const char*)buf->base, buf->len});
+    }
+
+    raft_id     id;
+    std::string content;
+
+};
+
+struct Worker : public tl::provider<Worker> {
+
+    Worker(tl::engine engine, raft_id raftID, const char* config = nullptr)
+    : tl::provider<Worker>(engine, 0)
+    , fsm{raftID}
+    , abtlog{id, ABT_IO_INSTANCE_NULL, config, engine.get_margo_instance()}
+    , raft{engine.get_margo_instance(), raftID, fsm, abtlog}
+    , id{raftID} {
+        #define DEFINE_WORKER_RPC(__rpc__) define("mraft_test_" #__rpc__, &Worker::__rpc__)
+        DEFINE_WORKER_RPC(bootstrap);
+        DEFINE_WORKER_RPC(start);
+        DEFINE_WORKER_RPC(add);
+        DEFINE_WORKER_RPC(assign);
+        DEFINE_WORKER_RPC(remove);
+        DEFINE_WORKER_RPC(apply);
+        DEFINE_WORKER_RPC(get_leader);
+        DEFINE_WORKER_RPC(transfer);
+        DEFINE_WORKER_RPC(suspend);
+        DEFINE_WORKER_RPC(get_fsm_content);
+        #undef DEFINE_WORKER_RPC
+    }
+
+    #define WRAP_CALL(func, ...)                   \
+    do {                                           \
+        try {                                      \
+            raft.func(__VA_ARGS__);                \
+        } catch (const mraft::RaftException& ex) { \
+            return ex.code();                      \
+        }                                          \
+        return 0;                                  \
+    } while (0)
+
+    int bootstrap(const std::vector<mraft::ServerInfo>& servers) {
+        WRAP_CALL(bootstrap, servers);
+    }
+
+    int start() {
+        WRAP_CALL(start);
+    }
+
+    int add(const raft_id& raftId, const std::string& addr) {
+        WRAP_CALL(add, raftId, addr.c_str());
+    }
+
+    int assign(const raft_id& raftId, const mraft::Role& role) {
+        WRAP_CALL(assign, raftId, role);
+    }
+
+    int remove(const raft_id& raftId) {
+        WRAP_CALL(remove, raftId);
+    }
+
+    int apply(const std::string& buffer) {
+        struct raft_buffer bufs = {
+            .base = (void*)buffer.c_str(),
+            .len  = buffer.size()
+        };
+        WRAP_CALL(apply, &bufs, 1U);
+    }
+
+    mraft::ServerInfo get_leader() const {
+        mraft::ServerInfo leaderInfo{0, ""};
+        try {
+            leaderInfo = raft.get_leader();
+        } catch (mraft::RaftException& ex) {}
+        return leaderInfo;
+    }
+
+    int transfer(raft_id transferToId) {
+        WRAP_CALL(transfer, transferToId);
+    }
+
+    void suspend(const tl::request& req, unsigned msec) const {
+        req.respond(0);
+        usleep(msec*1000);
+    }
+
+    const std::string& get_fsm_content() const {
+        return fsm.content;
+    }
+
+    #undef WRAP_CALL
+
+    raft_id         id;
+    FSM             fsm;
+    mraft::AbtIoLog abtlog;
+    mraft::Raft     raft;
+};
+
+// ----------------------------------------------------------------------------
 // Master functions and structure definitions
 // ----------------------------------------------------------------------------
 
 struct WorkerHandle {
-    raft_id      raftID = 0;
-    pid_t        pID    = -1;
-    tl::endpoint address;
+    raft_id             raftID = 0;
+    pid_t               pID    = -1;
+    tl::provider_handle address;
 
     auto to_string() const {
         std::stringstream stream;
@@ -63,6 +183,14 @@ struct WorkerHandle {
 struct Cluster {
 
     std::map<raft_id, WorkerHandle> workers;
+    tl::engine                      engine;
+    tl::remote_procedure            rpc_bootstrap;
+
+
+    Cluster(tl::engine engine)
+    : engine(engine)
+    , rpc_bootstrap(engine.define("mraft_test_bootstrap"))
+    {}
 
     auto to_string() const {
         std::stringstream stream;
@@ -72,6 +200,19 @@ struct Cluster {
         auto result = stream.str();
         if(!result.empty()) result.resize(result.size()-1);
         return result;
+    }
+
+    void bootstrap() const {
+        std::vector<mraft::ServerInfo> servers;
+        servers.reserve(workers.size());
+        for(auto& [id, worker] : workers)
+            servers.push_back({id, worker.address});
+        std::vector<tl::async_response> responses;
+        responses.reserve(workers.size());
+        for(auto& [id, worker] : workers)
+            responses.push_back(rpc_bootstrap.on(worker.address).async(servers));
+        for(auto& response : responses)
+            response.wait();
     }
 };
 
@@ -89,10 +230,14 @@ static std::optional<WorkerHandle> spawnWorker(MasterContext& master, const Opti
         return std::nullopt;
     }
 
+    auto raftID = master.nextRaftID++;
+
     pid_t pid = fork();
     if (pid < 0) {
         perror("Fork failed");
+        master.nextRaftID--;
         return std::nullopt;
+
     } else if (pid == 0) {
         // Child process
         close(pipefd[0]);
@@ -101,21 +246,25 @@ static std::optional<WorkerHandle> spawnWorker(MasterContext& master, const Opti
         auto engine = tl::engine(options.protocol, MARGO_SERVER_MODE);
         engine.enable_remote_shutdown();
 
+        auto worker = new Worker{engine, raftID};
+        engine.push_finalize_callback([worker](){ delete worker; });
+
         auto address = static_cast<std::string>(engine.self());
         address.resize(1024);
         ssize_t _ = write(pipefd[1], address.data(), 1024);
         close(pipefd[1]);
 
-        runWorker(options, engine);
+        engine.wait_for_finalize();
+        std::exit(0);
+
     } else {
         // Parent process
         close(pipefd[1]);
         char address[1024];
         ssize_t bytes_read = read(pipefd[0], address, 1024);
         close(pipefd[0]);
-        auto raftID = master.nextRaftID++;
         master.cluster->workers[raftID] = WorkerHandle{
-            raftID, pid, master.engine.lookup(address)
+            raftID, pid, tl::provider_handle{master.engine.lookup(address), 0}
         };
         return master.cluster->workers[raftID];
     }
@@ -126,7 +275,7 @@ static void runMaster(const Options& options) {
     master.engine = tl::engine{options.protocol, THALLIUM_CLIENT_MODE};
     master.engine.set_log_level(options.masterLogLevel);
     master.lua.open_libraries(sol::lib::base, sol::lib::string, sol::lib::math);
-    master.cluster = std::make_shared<Cluster>();
+    master.cluster = std::make_shared<Cluster>(master.engine);
 
     master.lua.new_usertype<Cluster>("Cluster",
         sol::meta_function::index, [](Cluster& cluster, raft_id id) -> std::optional<WorkerHandle> {
@@ -167,6 +316,17 @@ static void runMaster(const Options& options) {
         usleep(msec*1000);
     });
 
+    // initial cluster setup
+    for(size_t i = 0; i < options.initClusterSize; ++i) {
+        auto w = spawnWorker(master, options);
+        if(!w) {
+            margo_critical(master.engine.get_margo_instance(),
+                "Could not spawn initial worker %lu", i);
+            goto cleanup;
+        }
+    }
+    master.cluster->bootstrap();
+
     if(options.luaFile.empty()) {
         char* l;
         while((l = readline(">> ")) != nullptr) {
@@ -185,20 +345,11 @@ static void runMaster(const Options& options) {
         master.lua.script_file(options.luaFile);
     }
 
+cleanup:
     for(const auto& [raftID, worker] : master.cluster->workers) {
         master.engine.shutdown_remote_engine(worker.address);
         waitpid(worker.pID, nullptr, 0);
     }
-}
-
-// ----------------------------------------------------------------------------
-// Worker functionalities
-// ----------------------------------------------------------------------------
-
-[[noreturn]] static void runWorker(const Options& options, tl::engine engine) {
-    (void)options;
-    engine.wait_for_finalize();
-    std::exit(0);
 }
 
 // ----------------------------------------------------------------------------
@@ -266,6 +417,10 @@ static void parseCommandLine(int argc, char** argv, Options& options) {
         }
         if(!options.luaFile.empty() && !fs::is_directory(options.logPath)) {
             std::cerr << "error: " << options.logPath << " does not exist or not a directory" << std::endl;
+            exit(-1);
+        }
+        if(options.initClusterSize == 0) {
+            std::cerr << "error: cannot start with a cluster of size 0" << std::endl;
             exit(-1);
         }
 
