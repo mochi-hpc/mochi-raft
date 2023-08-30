6ec71d38a49e85d756eb3aea6860a05c74f1df83
Matthieu Dorier <mdorier@anl.gov>
2023-08-30 10:00:47 +0000

    fixing shutdown and kill tests

--- tests/scenarios/03-shutdown.lua
+++ tests/scenarios/03-shutdown.lua
@@ -1,7 +1,8 @@
 -- This test applies 3 entries to the state machine,
--- performs a barrier, shuts down a process, adds more
--- entries, then check that the content of the state
--- machine is as expected in all remaining workers.
+-- performs a barrier, adds a new process, adds more
+-- entries, removes the added process, adds more entries,
+-- then check that the content of the state machine is
+-- as expected in all remaining workers.
 
 print("[lua] Applying 3 commands")
 expected = ""
@@ -14,37 +15,44 @@ end
 print("[lua] Issuing barrier")
 cluster[1]:barrier()
 
-print("[lua] Checking who the leader is")
-leader = cluster[1]:get_leader()
-leader_id = leader.raft_id
+print("[lua] Creating new worker")
+new_worker = cluster.spawn()
+assert(new_worker.raft_id == 4)
 
-remove_id = leader_id + 1
-if remove_id == 4 then
-  remove_id = 1
-end
-print(string.format("[lua] Removing worker with id %d", remove_id))
-to_remove = cluster[remove_id]
-to_remove:shutdown()
-assert(#cluster == 2)
+print("[lua] Starting new worker")
+new_worker:start()
 
-print("[lua] Applying 2 more commands")
-for i=1,2 do
+print("[lua] Adding the new worker to the cluster")
+cluster[1]:add(new_worker)
+
+print("[lua] Assigning the new worker as voter")
+cluster[1]:assign(new_worker, Role.VOTER)
+
+print("[lua] Applying 4 more commands")
+for i=1,4 do
   local entry = string.format("entry_%d", i+3)
-  cluster[leader_id]:apply(entry)
+  cluster[i]:apply(entry)
   expected = expected .. entry
 end
 
 print("[lua] Issuing barrier")
 cluster[1]:barrier()
 
+print("[lua] Shutting down worker")
+new_worker:shutdown()
+assert(#cluster == 3)
+
+print("[lua] Applying 3 commands")
+for i=1,3 do
+  local entry = string.format("entry_%d", i+7)
+  cluster[i]:apply(entry)
+  expected = expected .. entry
+end
+
 print("[lua] Checking the content of the state machines")
 print("[lua] Expected content: " .. expected)
 for i=1,3 do
-  if i == remove_id then
-    print(string.format("[lua] Worker %d was removed, not checking its content", i))
-  else
-    content = cluster[i]:get_fsm_content()
-    print(string.format("[lua] Content in worker %d is %s (length %d)", i, content, #content))
-    assert(content == expected)
-  end
+  content = cluster[i]:get_fsm_content()
+  print(string.format("[lua] Content in worker %d is %s (length %d)", i, content, #content))
+  assert(content == expected)
 end
