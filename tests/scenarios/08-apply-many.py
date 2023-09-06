9a5a13e1db8cd357cd20093fca2a0845a5fc6ae4
Matthieu Dorier <mdorier@anl.gov>
2023-09-06 14:17:03 +0100

    added a test to produce multiple entry fiels in abt-io log

---
+++ tests/scenarios/08-apply-many.py
@@ -0,0 +1,28 @@
+# This test applies 10 entries to the state machine,
+# performs a barrier, then checks the content of the
+# state machine. This test is meant to force the abt-io 
+# log backend to create a second entry file (entry files
+# are limited to 1MB by default).
+from raft import cluster
+import time
+import random
+import string
+
+def generate_random_string(n):
+    characters = string.printable
+    random_string = ''.join(random.choice(characters) for _ in range(n))
+    return random_string
+
+expected = ""
+for i in range(0, 10):
+    entry = generate_random_string(20*1024) # 20 KB entry
+    cluster[(i % 3) + 1].apply(entry)
+    expected = expected + entry
+
+cluster[1].barrier()
+
+time.sleep(1)
+
+for i in range(0,3):
+  content = cluster[i+1].get_fsm_content()
+  assert content == expected, f"in worker {i+1} content differs from expected"
